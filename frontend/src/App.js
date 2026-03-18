5f66b4382af3d323fc2ec483a57e8108f20fd48a
Deepakragavan1013 <cf012deepakragavan@gmail.com>
2026-03-18 17:24:47 +0530

    eslint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
